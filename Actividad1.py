fe2eefb7a24ae19a769a6ad00e16e74b3a2ca22f
waltercamino <waltercamno@hotmail.com>
2022-06-24 18:13:18 -0300

    corregi funcion suma

--- Actividad1.py
+++ Actividad1.py
@@ -1,3 +1,8 @@
+from distutils.command.clean import clean
+from re import X
+from unittest import result
+
+
 def Minimo(lista): #funsion que calcula el minimo de la litsa
     min = lista[0]
     for elemento in range (5):
@@ -5,21 +10,30 @@ def Minimo(lista): #funsion que calcula el minimo de la litsa
             min = lista[elemento]
     print(f"El elemento menor es: {min}" ) 
 
+def Sumar(lista):
+    suma=0
+    for numero in lista:
+        suma += numero
+    return suma
+
 lista = [] #lista vacia 
-print ("INGRESE 5 NUMEROS ENTEROS")
+print ("INGRESE 5 NUMEROS ENTEROS\n")
+
 for  elemento in range(5): #Llena la lista
     lista.append(int(input(f"Ingrese el numero {elemento + 1}: ")))
+print()
 #Menu de opciones
 print ("""Que desa hacer con ellos:
     1 - Sumarlos
     2 - Obtener Su Promedio
     3 - Encontrar El Numero Mas Grande
-    4 - Encontrar El Numero Mas Pequeño""")
+    4 - Encontrar El Numero Mas Pequeño\n""")
 
 a = int(input("Ingrese su Opcion: "))
 if a == 1:
-    suma=sum(lista)
-    print=("el resultado de la sumas es: ",suma) #Suma()
+    print()
+    print("La suma de los numeros ingresados es igual a: ",Sumar(lista)) 
+    #Suma()
 elif a == 2:
     print("Aca va la funcion")
     #Promedio()
@@ -28,4 +42,3 @@ elif a == 3:
     #Maximo()
 elif a == 4:
     Minimo(lista)#Llama a la funcion minimo y le pasa como argumanto la lista creada
-    
