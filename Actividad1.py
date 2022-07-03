361e4dc221166e46068164bc9681604ecc58b62a
alantoro22 <106613099+alantoro22@users.noreply.github.com>
2022-07-03 19:02:47 -0300

    Update Actividad1.py

--- Actividad1.py
+++ Actividad1.py
@@ -36,6 +36,17 @@ if a == 1:
     #Suma()
 elif a == 2:
     print("Aca va la funcion")
+    n= int(input("ingrese la cantidad de notas a promediar"))
+    suma=0
+    i=1
+    while(i<= n):
+        print("ingrese la nota numero:",i)
+        nota=float(input())
+        suma=suma+nota
+        i+=1
+        prom = suma/n
+        print("El promedio de notas es:",prom)
+        
     #Promedio()
 elif a == 3:
     print("Aca va la funcion")
