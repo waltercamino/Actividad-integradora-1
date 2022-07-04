806ff3673e45be2edef35009a04529d3d5f2d62d
conichan <conichan139@gmail.com>
2022-07-04 10:28:38 -0300

    ultmi 2

--- Actividad1.py
+++ Actividad1.py
@@ -1,13 +1,15 @@
 import os
 import time
 
+#Funsion Sumar
 def Sumar(lista):
     suma  = 0
     for numero in lista:
         suma += numero
     return suma
 
-def Minimo(lista): #funsion que calcula el minimo de la litsa
+#Funsion Minimo
+def Minimo(lista): 
     min = lista[0]
     for elemento in range (5):
         if lista[elemento] < min:
@@ -15,7 +17,9 @@ def Minimo(lista): #funsion que calcula el minimo de la litsa
     os.system("cls")
     print(f"""            ---------------------------------
             |   El numero mas pequeño es: {min} |    
-            ---------------------------------""" )            
+            ---------------------------------""" )    
+
+#Funsion Menu
 def MenuPrincipal ():
     #Menu de opciones
     print ("""            ---------------------------------
@@ -29,55 +33,6 @@ def MenuPrincipal ():
             |        Mas Pequeño            |  
             |    0 - Salir                  |
             ---------------------------------""")
-
-<<<<<<< HEAD
-    return input("                 Ingrese su Opcion: ")
-
-def CargarLista (lista):
-    print ("""            ---------------------------------
-            |   INGRESE 5 NUMEROS ENTEROS   |
-            ---------------------------------""")
-    for  elemento in range(5): #Llena la lista
-        lista.append(int(input(f"                  Ingrese el numero {elemento + 1}: ")))
-
-lista = [] #lista bacia 
-CargarLista(lista)
-while True:
-    os.system("cls")
-    a = MenuPrincipal ()
-    if a == "1":
-        os.system("cls")
-        print(F"""        ---------------------------------------------------------
-        |   La suma de los numeros ingresados es igual a: {Sumar(lista)}    |
-        ---------------------------------------------------------""")
-        time.sleep(5)#detener para que se vea resultado
-    elif a == "2":
-        print("Aca va la funcion")
-        #Promedio()
-    elif a == "3":
-        print("Aca va la funcion")
-        #Maximo()
-    elif a == "4":
-        Minimo(lista)#Llama a la funcion minimo y le pasa como argumanto la lista creada
-        time.sleep(5)#detener para que se vea el resultado
-    elif a == "0":
-        os.system("cls")
-        print ("""        -----------------------------------------
-        | ¡¡GRACIAS POR USAR NUESTRO PROGRAMA!! |
-        -----------------------------------------""")
-        break
-    else: 
-        os.system("cls")
-        print ("""                  ----------------------
-                  | OPCION NO ADMITIDA |
-                  ----------------------""")
-        time.sleep(5)          
-=======
-def Sumar(lista):
-    suma=0
-    for numero in lista:
-        suma += numero
-    return suma
 
 lista = [] #lista vacia 
 print ("INGRESE 5 NUMEROS ENTEROS\n")
@@ -116,4 +71,3 @@ elif a == 3:
     #Maximo()
 elif a == 4:
     Minimo(lista)#Llama a la funcion minimo y le pasa como argumanto la lista creada
->>>>>>> 361e4dc221166e46068164bc9681604ecc58b62a
