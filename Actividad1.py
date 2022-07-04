d03628cc8295b5e12f5d84181d5407a11878d023
conichan <conichan139@gmail.com>
2022-07-04 10:20:46 -0300

    ultimo cambio

--- Actividad1.py
+++ Actividad1.py
@@ -1,40 +1,73 @@
-#print("ingrese 5 numeros enteros")
-#num1= int(input(print("Ingrese el primer numero: ")))
-#num2= int(input(print("Ingrese el segundo numero: ")))
-#num3= int(input(print("Ingrese el tercer numero: ")))
-#num4= int(input(print("Ingrese el cuarto numero: ")))
-#num5= Int(input(print("Ingrese el quinto numero: ")))
-#print()
-#rint("Usted encontrara el resultado de 5 funciones sobre esto numeros que ingreso")
-#list(num1,num2, num3, num4,num5)
+import os
+import time
+
+def Sumar(lista):
+    suma  = 0
+    for numero in lista:
+        suma += numero
+    return suma
 
 def Minimo(lista): #funsion que calcula el minimo de la litsa
     min = lista[0]
     for elemento in range (5):
         if lista[elemento] < min:
             min = lista[elemento]
-    print(f"El elemento menor es: {min}" ) 
+    os.system("cls")
+    print(f"""            ---------------------------------
+            |   El numero mas pequeño es: {min} |    
+            ---------------------------------""" )            
+def MenuPrincipal ():
+    #Menu de opciones
+    print ("""            ---------------------------------
+            |   ¿QUE DESEA HACER CON ELLOS? |
+            ---------------------------------
+            |    1 - Sumarlos               |
+            |    2 - Obtener Su Promedio    |
+            |    3 - Encontrar El Numero    | 
+            |        Mas Grande             |      
+            |    4 - Encontrar El Numero    | 
+            |        Mas Pequeño            |  
+            |    0 - Salir                  |
+            ---------------------------------""")
+
+    return input("                 Ingrese su Opcion: ")
+
+def CargarLista (lista):
+    print ("""            ---------------------------------
+            |   INGRESE 5 NUMEROS ENTEROS   |
+            ---------------------------------""")
+    for  elemento in range(5): #Llena la lista
+        lista.append(int(input(f"                  Ingrese el numero {elemento + 1}: ")))
 
 lista = [] #lista bacia 
-print ("INGRESE 5 NUMEROS ENTEROS")
-for  elemento in range(5): #Llena la lista
-    lista.append(int(input(f"Ingrese el numero {elemento + 1}: ")))
-#Menu de opciones
-print ("""Que desa hacer con ellos:
-    1 - Sumarlos
-    2 - Obtener Su Promedio
-    3 - Encontrar El Numero Mas Grande
-    4 - Encontrar El Numero Mas Pequeño""")
-
-a = int(input("Ingrese su Opcion: "))
-if a == 1:
-    print("Aca va la funcion")
-    #Suma()
-elif a == 2:
-    print("Aca va la funcion")
-    #Promedio()
-elif a == 3:
-    print("Aca va la funcion")
-    #Maximo()
-elif a == 4:
-    Minimo(lista)#Llama a la funcion minimo y le pasa como argumanto la lista creada
+CargarLista(lista)
+while True:
+    os.system("cls")
+    a = MenuPrincipal ()
+    if a == "1":
+        os.system("cls")
+        print(F"""        ---------------------------------------------------------
+        |   La suma de los numeros ingresados es igual a: {Sumar(lista)}    |
+        ---------------------------------------------------------""")
+        time.sleep(5)#detener para que se vea resultado
+    elif a == "2":
+        print("Aca va la funcion")
+        #Promedio()
+    elif a == "3":
+        print("Aca va la funcion")
+        #Maximo()
+    elif a == "4":
+        Minimo(lista)#Llama a la funcion minimo y le pasa como argumanto la lista creada
+        time.sleep(5)#detener para que se vea el resultado
+    elif a == "0":
+        os.system("cls")
+        print ("""        -----------------------------------------
+        | ¡¡GRACIAS POR USAR NUESTRO PROGRAMA!! |
+        -----------------------------------------""")
+        break
+    else: 
+        os.system("cls")
+        print ("""                  ----------------------
+                  | OPCION NO ADMITIDA |
+                  ----------------------""")
+        time.sleep(5)          
